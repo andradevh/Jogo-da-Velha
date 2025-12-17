fcd2313718b4e9ada4856e27997e4e9ba88d46f0
andradevh <andrade.vh18@gmail.com>
2025-12-16 22:36:49 -0300

    jogo da velha quase pronto, falta diagonais

--- src/App.java
+++ src/App.java
@@ -1,5 +1,178 @@
+import java.util.Scanner;
+
 public class App {
+    public static Scanner sc;
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        sc = new Scanner(System.in);
+        char[][] arena = new char[3][3];
+        while (true) {
+            System.out.println("---Turno X---");
+            imprimirMatriz(arena);
+            popularMatrizX(arena);
+
+            if (checarVitoriaX(arena)) {
+                System.out.println("Vitória do Player X!");
+                imprimirMatriz(arena);
+                break;
+            } else if (checarVitoriaO(arena)) {
+                System.out.println("---Turno O---");
+                imprimirMatriz(arena);
+                break;
+            } else if (checarEmpate(arena)) {
+                System.out.println("Deu velha!");
+                imprimirMatriz(arena);
+                break;
+            }
+            System.out.println("Turno do O");
+            imprimirMatriz(arena);
+            popularMatrizO(arena);
+
+            if (checarVitoriaX(arena)) {
+                System.out.println("Vitória do Player X!");
+                imprimirMatriz(arena);
+                break;
+            } else if (checarVitoriaO(arena)) {
+                System.out.println("Vitória do Player O!");
+                imprimirMatriz(arena);
+                break;
+            } else if (checarEmpate(arena)) {
+                System.out.println("Deu velha!");
+                imprimirMatriz(arena);
+                break;
+            }
+        }
     }
+
+    public static void popularMatrizX(char[][] arena) {
+        int x, y;
+
+        while (true) {
+            System.out.println("Insira a coordenada que deseja jogar");
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            if (arena[x][y] != 0) {
+                System.out.println("Essa casa já foi escolhida!");
+            } else {
+                arena[x][y] = 'X';
+                break;
+            }
+        }
+
+    }
+
+    public static void popularMatrizO(char[][] arena) {
+        int x, y;
+
+        while (true) {
+            System.out.println("Insira a coordenada que deseja jogar");
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            if (arena[x][y] != 0) {
+                System.out.println("Essa casa já foi escolhida!\n");
+            } else {
+                arena[x][y] = 'O';
+                break;
+            }
+        }
+    }
+
+    public static void imprimirMatriz(char[][] arena) {
+        for (int i = 0; i < arena.length; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < arena[i].length; j++) {
+                char elemento = arena[i][j];
+                String display = (elemento == 0) ? " " : String.valueOf(elemento);
+                System.out.print(display + " | ");
+            }
+
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    public static boolean checarEmpate(char arena[][]) {
+
+        for (int i = 0; i < arena.length; i++) {
+            for (int j = 0; j < arena.length; j++) {
+                if (arena[i][j] == 0) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
+    }
+
+    public static boolean checarVitoriaX(char arena[][]) {
+        boolean linhaVencedora;
+        boolean colunaVencedora;
+        for (int i = 0; i < arena.length; i++) {
+            linhaVencedora = true;
+            for (int j = 0; j < arena.length; j++) {
+                if (arena[i][j] != 'X') {
+                    linhaVencedora = false;
+                    break;
+                }
+            }
+
+            if (linhaVencedora) {
+                return true;
+
+            }
+        }
+        for (int j = 0; j < arena.length; j++) {
+            colunaVencedora = true;
+            for (int i = 0; i < arena.length; i++) {
+                if (arena[i][j] != 'X') {
+                    colunaVencedora = false;
+                    break;
+                }
+            }
+            if (colunaVencedora) {
+                return true;
+
+            }
+
+        }
+        return false;
+    }
+
+    public static boolean checarVitoriaO(char arena[][]) {
+        boolean linhaVencedora;
+        boolean colunaVencedora;
+        for (int i = 0; i < arena.length; i++) {
+            linhaVencedora = true;
+            for (int j = 0; j < arena.length; j++) {
+                if (arena[i][j] != 'O') {
+                    linhaVencedora = false;
+                    break;
+                }
+            }
+
+            if (linhaVencedora) {
+                return true;
+
+            }
+        }
+        for (int j = 0; j < arena.length; j++) {
+            colunaVencedora = true;
+            for (int i = 0; i < arena.length; i++) {
+                if (arena[i][j] != 'O') {
+                    colunaVencedora = false;
+                    break;
+                }
+            }
+            if (colunaVencedora) {
+                return true;
+
+            }
+
+        }
+        return false;
+    }
+
 }
